b6ab920b32e9748a610bcb2003bc7edcf6eb81e2
Harish <harish@infeedo.com>
2018-01-21 20:53:09 +0530

    fix function

--- script/func.js
+++ script/func.js
@@ -1,55 +1,51 @@
 alert();
-$(document).ready(function()
-{
-	$('#p1').click(function () 
-		
-	{ 
-		$('#p1').hide();
-	} )
-   $("#1").click(function()
-   {
+$(document).ready(function () {
+    $('#p1').click(function () {
+        $('#p1').hide();
+    })
+    $("#1").click(function () {
         $("#p1").css("background-color", "yellow");
-        $("#1").addClass("ColorChange");
+        // $("#1").addClass("ColorChange");
         $("#p1").addClass("Heading");
-        $("#2").removeClass("ColorChange");
-        $("#3").removeClass("ColorChange");
+        // $("#2").removeClass("ColorChange");
+        // $("#3").removeClass("ColorChange");
 
     })
-      $("#2").click(function()
-   {
+    $("#2").click(function () {
         $("#p1").css("background-color", "red");
-        $("#2").addClass("active");
-        $("#1").removeClass("ColorChange");
-        $("#3").removeClass("ColorChange");
+        // $("#2").addClass("active");
+        // $("#1").removeClass("ColorChange");
+        // $("#3").removeClass("ColorChange");
 
     })
-         $("#3").click(function()
-   {
+    $("#3").click(function () {
         $("#p1").css("background-color", "blue");
-        $("#3").addClass("ColorChange");
-        $("#1").removeClass("ColorChange");
-        $("#2").removeClass("ColorChange");
+        // $("#3").addClass("ColorChange");
+        // $("#1").removeClass("ColorChange");
+        // $("#2").removeClass("ColorChange");
 
     })
 
-       $("#Home").click(function(){
-        
+    $("#Home").click(function () {
+
         $("#p1").addClass("Heading");
-    })
+    });
 
+
+
+    
+    
+    
+    
+    
+    // document.getElementById("p1").innerHTML = "New text";
+    
 })
-  
-
-
-
-
-document.getElementById("p1").innerHTML = "New text";
-
-function myFunc() 
-{
-    document.getElementById("p1").style.color = "black";
-
-    document.getElementById("p1").innerHTML = "Home";
-
-    document.getElementById("1").removeClass = "active";
-}
+        function myFunc(id) {
+            $('#'+id).addClass('active');
+            [1,2,3].forEach(function(num) {
+                if(id != num) {
+                    $('#'+num).removeClass('active');
+                }
+            })
+        }
